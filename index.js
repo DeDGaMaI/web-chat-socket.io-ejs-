bf816af0032d93e0b42d1fd0ba99fbc0880c0938
Oleg Kachanouski <kachanouski.ka@gmail.com>
2022-01-16 15:33:11 +0300

    Saved to git repo

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app =  express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require("fs");
+var d = new Date()
+var time = d.getMonth() + " " + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+var online = 0
+
+app.get('/', (req, res) =>{
+    res.render("index.ejs");
+})
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat..</i>');
+		fs.appendFileSync("chat.txt", "[" + time + "]" + socket.username + " joined the chat..\n");
+		online = online + 1;
+		console.log(online);
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+		fs.appendFileSync("chat.txt", "[" + time + "]" + socket.username + " left the chat..\n");
+		online = online - 1;
+    })
+
+    socket.on('chat_message', function(message) {
+		if(message == "online")
+		{
+			io.emit('chat_message',"[SERVER]: " + online);
+		}
+		if(message.startsWith("/write"))
+		{
+			io.emit('chat_message', "[SERVER]: " + message.slice(7, message.length));
+		}
+		else {
+	        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+			fs.appendFileSync("chat.txt", "[" + time + "]" + socket.username + ":" + message + "\n");
+		}
+	});
+
+});
+
+const server = http.listen(8080, ()=>{
+    console.log("listening on port : 8080");
+})
